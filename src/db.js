72d899e0691849a9d4d0ada6f9ffa480a8ed75c2
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-05-14 14:07:47 -0500

    Base de datos lista en la nube

--- src/db.js
+++ src/db.js
@@ -1,24 +1,14 @@
 /* file for data base connection (postgresql) */
 import pg from "pg";
-import { db } from "./config.js";
+//import { db } from "./config.js";
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: "postgres://default:FNS5KHhBmAi3@ep-empty-bonus-a4b0x0gj-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  connectionString: process.env.POSTGRES_URL,
 })
 
-// export const pool = new pg.Pool({
-//   user: db.user,
-//   password: db.password,
-//   host: db.host,
-//   port: db.port,
-//   database: db.database,
-// });
-
-//pool.on("connect", () => console.log("DB connected"));
 pool.connect((err) => {
     if (err) throw err;
     console.log("Conexion a la base de datos completada");
 })
 
-//module.exports = pool;
