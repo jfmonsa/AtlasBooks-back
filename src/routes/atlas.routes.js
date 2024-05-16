b29c99e60a931480009913ea315ea0d896b8b6d1
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-05-15 23:30:41 -0500

    Inicio cambiar contraseña

--- src/routes/atlas.routes.js
+++ src/routes/atlas.routes.js
@@ -11,13 +11,18 @@
 // app.get("/api/user", (req, res) => {
 //   res.json({ users: ["user1", "user2", "user3", "user4"] });
 // });
-
-import { Router } from "express";
+import { Router } from 'express';
 import { register, login } from "../controllers/atlas.controller.js";
+import { current_user, change_password } from '../controllers/myaccount.controller.js';
+
 const router = Router();
 
 router.post('/register', register)
 router.post('/login', login)
+
+//myaccount routes
+router.get('/myaccount/current-user', current_user); // /api/current-user
+router.post('/myaccount/change-password', change_password);   // /api/change-password
 
 //routes here
 export default router;
