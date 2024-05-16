8b2e7dd08e15f7f405d5ae338485b6a5bfff2b3c
jfmonsa <juanmonsalve@protonmail.com>
2024-05-16 14:32:07 -0500

    adding router for lists

--- src/routes/lists.js
+++ src/routes/lists.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  res.send({ message: "ruta de los libros" });
+  res.send({ message: "ruta de las listas" });
 });
 
 export default router;
