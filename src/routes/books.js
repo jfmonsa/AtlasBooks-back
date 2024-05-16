d5d73f368be95ccd6a11be390880075fd4ea1d05
jfmonsa <juanmonsalve@protonmail.com>
2024-05-16 16:59:13 -0500

    Adding controllers and routes for Books crud, also a default profile pic image for new users

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { getBooks, getBook, createBook } from "../controllers/books.js";
 const router = Router();
 
-router.get("/", async (req, res) => {
-  res.send({ message: "ruta de los libros" });
-});
+router.get("/", getBooks);
+router.get("/:id", getBook);
+router.post("/", createBook);
 
 export default router;
