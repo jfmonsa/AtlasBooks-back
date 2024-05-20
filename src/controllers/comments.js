360009f3e290edeb7ce48ceea3a5b5e45e50ef00
RamSterB <113079935+RamSterB@users.noreply.github.com>
2024-05-20 17:48:26 -0500

    Create comments routes and controller

---
+++ src/controllers/comments.js
@@ -0,0 +1,108 @@
+import { pool } from "../db.js";
+
+export const getComments = async (req, res) => {
+  try {
+    //We get the id of the book from the parameters (id of the book)
+    const { bookId } = req.params;
+    //We get all the comments from the database that are related to the book
+    const { rows } = await pool.query(`
+        SELECT 
+            (U.nickname, U.pathprofilepic, C.idcoment, C.iduser, C.idbook, C.datec, C.textc) 
+        FROM users U JOIN book_comment C ON U.id = C.iduser
+        WHERE idbook = $1 
+        ORDER BY C.datec`, [bookId]);
+    //We return the comments
+    res.json(rows);
+  } catch (error) {
+    res.status(500).send([error.message]);
+  }
+};
+export const createComment = async (req, res) => {
+  try {
+    //On the body of the request, we expect to receive the text of the comment and the id of the book
+    const { text, bookId } = req.body;
+    //We get the id of the user from the session
+    const { id } = req.user;
+    //We get the current date
+    const date = new Date();
+    //We insert the comment into the database
+    const { rows } = await pool.query(`
+    INSERT INTO book_comment (iduser, idbook, datec,textc) 
+    VALUES ($1, $2, $3, $4) RETURNING *`,
+      [id, bookId, date, text]
+    );
+    //We return the comment that was created
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).send([error.message]);
+  }
+};
+export const getComment = async (req, res) => {
+  try {
+    //We get the id of the comment from the parameters (id of the comment)
+    const { id } = req.params;
+    //We search for the comment in the database
+    const { rows } = await pool.query(
+      "SELECT * FROM book_comment WHERE idcoment = $1",
+      [id]
+    );
+    //If the comment does not exist, we return an error
+    if (rows.length === 0) {
+      return res.status(404).send(["Comment not found"]);
+    }
+    //We return the comment
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).send([error.message]);
+  }
+};
+export const updateComment = async (req, res) => {
+  try {
+    //We get the id of the comment from the parameters (id of the comment)
+    const { id } = req.params;
+    //We get the text of the comment from the body of the request
+    const { text } = req.body;
+    //We search for the comment in the database
+    const { rows } = await pool.query(
+      "SELECT * FROM book_comment WHERE idcoment = $1",
+      [id]
+    );
+    //If the comment does not exist, we return an error
+    if (rows.length === 0) {
+      return res.status(404).send(["Comment not found"]);
+    }
+    //We update the comment in the database
+    const { rows: updatedRows } = await pool.query(
+      "UPDATE book_comment SET textc = $1 WHERE idcoment = $2 RETURNING *",
+      [text, id]
+    );
+    //We return the comment that was updated
+    res.json(updatedRows[0]);
+  } catch (error) {
+    res.status(500).send([error.message]);
+  }
+};
+export const deleteComment = async (req, res) => {
+  try {
+    //We get the id of the comment from the parameters (id of the comment)
+    const { id } = req.params;
+    //We search for the comment in the database
+    const { rows } = await pool.query(
+      "SELECT * FROM book_comment WHERE idcoment = $1",
+      [id]
+    );
+    //If the comment does not exist, we return an error
+    if (rows.length === 0) {
+      return res.status(404).send(["Comment not found"]);
+    }
+    //We delete the comment from the database
+    await pool.query(
+      "DELETE FROM book_comment WHERE idcoment = $1 RETURNING *",
+      [false, id]
+    );
+    //We return the comment that was deleted
+    res.status(200).send(["Comment deleted successfully"]);
+  } catch (error) {
+    res.status(500).send([error.message]);
+  }
+};
