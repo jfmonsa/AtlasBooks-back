b40be9952dba377a3b53fef092f8a040c4f1bd95
LeiderCortes <leidersantiago27@gmail.com>
2024-05-23 15:11:09 -0500

    epa

--- src/routes/lists.js
+++ src/routes/lists.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 const router = Router();
-import { createList } from "../controllers/lists.js";
-
+import { createList, getList } from "../controllers/lists.js";
+/*
 router.get("/", async (req, res) => {
   res.send({ message: "ruta de las listas" });
 });
-
+*/
 router.post("/", createList);
-
+router.get("/:id", getList);
+router.get("/", getList);
 export default router;
