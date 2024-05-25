0f4c92f425b217bb0b8e4b94892cd648ec19b5bc
LeiderCortes <leidersantiago27@gmail.com>
2024-05-25 16:42:12 -0500

    lists get all

--- src/controllers/lists.js
+++ src/controllers/lists.js
@@ -1,76 +1,93 @@
 import { pool } from "../db.js";
 
-  /**
+/**
  * Create a list in the db
  * @param {*} req
  * @param {*} res
  */
 export const createList = async (req, res) => {
-    try {
-      // Getting data from the request body
-      const { 
-        title, 
-        descriptionL, 
-        dateL, 
-        idUserCreator, 
-        isPublic 
-    } = req.body;
-  
-      // Preparing values for the query
-      const query_values = [
-        title,
-        descriptionL,
-        dateL,
-        idUserCreator,
-        isPublic
-    ];
-  
-      // Inserting the new list into the LIST table
-      const newList_query = await pool.query(
-        "INSERT INTO BOOK_LIST (title, descriptionL, dateL, idUserCreator, isPublic) VALUES ($1, $2, $3, $4, $5) RETURNING id",
-        query_values
-      );
-  
-      // Getting the auto-incremented id of the new list
-      const listId = newList_query.rows[0].id;
-  
-      // Sending response
-      res.status(201).send({ message: "List created successfully", listId });
-    } catch (error) {
-      console.error("Error creating list:", error);
-      res.status(500).send({ error: "Failed to create list" });
-    }
-  };
-  
-  /**
- * Get a single book information by its id from the data base
+  try {
+    // Getting data from the request body
+    const { title, descriptionL, dateL, idUserCreator, isPublic } = req.body;
+
+    // Preparing values for the query
+    const query_values = [title, descriptionL, dateL, idUserCreator, isPublic];
+
+    // Inserting the new list into the LIST table
+    const newList_query = await pool.query(
+      "INSERT INTO BOOK_LIST (title, descriptionL, dateL, idUserCreator, isPublic) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+      query_values
+    );
+
+    // Getting the auto-incremented id of the new list
+    const listId = newList_query.rows[0].id;
+
+    // Sending response
+    res.status(201).send({ message: "List created successfully", listId });
+  } catch (error) {
+    console.error("Error creating list:", error);
+    res.status(500).send({ error: "Failed to create list" });
+  }
+};
+
+/**
+ * Get a single list information by its id from the database
  * @param {*} req
  * @param {*} res
  */
 export const getList = async (req, res) => {
   try {
     const idList = req.params.id;
-    const query_list = await pool.query("SELECT * FROM BOOK_LIST WHERE id = $1", [
-      idList,
-    ]);
 
-    // Si no se encuentra la lista, devolver un error 404
+    // Get the list details
+    const query_list = await pool.query(
+      "SELECT * FROM BOOK_LIST WHERE id = $1",
+      [idList]
+    );
+
+    // If the list is not found, return a 404 error
     if (query_list.rows.length === 0) {
       return res.status(404).json({ error: "List not found" });
     }
 
-    
-    //Send response
-    res.status(201).send({
-      idList,
+    // Get book IDs in the list
+    const query_books_in_list = await pool.query(
+      "SELECT idBook FROM BOOK_IN_LIST WHERE idList = $1",
+      [idList]
+    );
+    const bookIds = query_books_in_list.rows.map((row) => row.idbook);
+
+    // Get details of books and their authors
+    let books = [];
+    if (bookIds.length > 0) {
+      const query_books_details = await pool.query(
+        `SELECT b.id, b.title, b.pathbookcover, array_agg(ba.author) AS authors
+         FROM BOOK b
+         INNER JOIN BOOK_AUTHORS ba ON b.id = ba.idbook
+         WHERE b.id = ANY($1::int[])
+         GROUP BY b.id`,
+        [bookIds]
+      );
+      books = query_books_details.rows.map((book) => {
+        return {
+          bookId: book.id,
+          title: book.title,
+          pathBookCover: book.pathbookcover,
+          authors: book.authors.join(", "),
+        };
+      });
+    }
+
+    // Send response
+    return res.status(200).json({
       title: query_list.rows[0].title,
-      descriptionL: query_list.rows[0].descriptionl,
-      dateL: query_list.rows[0].datel,
-      idUserCreator: query_list.rows[0].idusercreator,
+      description: query_list.rows[0].descriptionl,
+      date: query_list.rows[0].datel,
       isPublic: query_list.rows[0].ispublic,
+      books: books,
     });
   } catch (error) {
-    console.error("Error creating book:", error);
-    res.status(500).send({ error: "Error getting the book" });
+    console.error("Error getting list:", error);
+    res.status(500).send({ error: "Error getting the list" });
   }
 };
--- src/routes/lists.js
+++ src/routes/lists.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 const router = Router();
 import { createList, getList } from "../controllers/lists.js";
-/*
-router.get("/", async (req, res) => {
-  res.send({ message: "ruta de las listas" });
-});
-*/
+
+// Ruta para crear una lista
 router.post("/", createList);
+
+// Ruta para obtener una lista por su ID
 router.get("/:id", getList);
-router.get("/", getList);
+
+// Eliminar esta ruta ya que no tiene sentido tener una ruta get "/" que se solapa con get("/:id")
+// router.get("/", getList);
+
 export default router;
