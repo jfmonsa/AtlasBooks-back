80458399f96880d5f0f1919aac2fe82ddae08315
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-06-11 22:53:05 -0500

    Agregar netlify a las rutas permitidas por cors

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,17 +1,18 @@
 const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',').map((origin) => origin.trim())
+  ? process.env.ALLOWED_ORIGINS.split(",").map((origin) => origin.trim())
   : [
-      'https://atlas-books-back.vercel.app',
-      'http://localhost:5173',
+      "https://atlas-books-back.vercel.app",
+      "http://localhost:5173",
+      "https://atlasbooks.netlify.app",
     ];
 
 const corsOptions = {
   origin: (origin, callback) => {
     allowedOrigins.includes(origin) || !origin
       ? callback(null, true)
-      : callback(new Error('Not allowed by CORS').message);
+      : callback(new Error("Not allowed by CORS").message);
   },
   credentials: true,
 };
 
-export default corsOptions
+export default corsOptions;
