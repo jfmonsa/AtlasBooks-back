966bb7b7ee04caa2fdfbaca6f9fce8a7a2d153e1
RamSterB <113079935+RamSterB@users.noreply.github.com>
2024-06-15 17:26:28 -0500

    add getRate function in rateBook controller

--- src/routes/rateBook.js
+++ src/routes/rateBook.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { rateBook } from "../controllers/rateBook.js";
+import { rateBook,getRate } from "../controllers/rateBook.js";
 import { authMiddleware } from "../middleware/session.js";
 
 const router = Router();
 
-router.post("/", authMiddleware ,rateBook); //  http://localhost:3000/api/reports
-
+router.post("/", authMiddleware ,rateBook); //  http://localhost:3000/api/rateBook
+router.get("/:idbook", authMiddleware ,getRate); //  http://localhost:3000/api/reports
 
 export default router;
