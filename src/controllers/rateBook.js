6f9a8b4e5823a4e9c5ab6ef3ff42edb4d225bb35
jfmonsa <juanmonsalve@protonmail.com>
2024-06-16 13:32:24 -0500

    Solving minor bug with unrated books

--- src/controllers/rateBook.js
+++ src/controllers/rateBook.js
@@ -35,15 +35,21 @@ export const rateBook = async (req, res) => {
 
 export const getRate = async (req, res) => {
   try {
-    const  idbook  = req.params.idbook;
+    const idbook = req.params.idbook;
     const { id } = req.user;
-    
+
     //Get the rate
     const rate = await pool.query(
       "SELECT ratevalue FROM book_rate WHERE iduser = $1 AND idbook = $2",
       [id, idbook]
-    )
+    );
 
+    let response;
+    if (rate.rows.length == 0) {
+      response = { ratevalue: 0 };
+    } else {
+      response = rate.rows[0];
+    }
     //Return the response
     res.status(200).json(rate.rows);
   } catch (error) {
--- src/controllers/userLists.js
+++ src/controllers/userLists.js
@@ -1,41 +1,42 @@
 import { pool } from "../db.js";
 
 export const userLists = async (req, res) => {
-    try {
-        const { id } = req.query;
+  try {
+    const { id } = req.query;
 
-        if (!id) {
-            return res
-                .status(400)
-                .json({ error: true, message: "Missing required parameters" });
-        }
+    if (!id) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Missing required parameters" });
+    }
 
-        const userListsQuery = await pool.query(`
+    const userListsQuery = await pool.query(
+      `
             SELECT 
+            id,
             title,
             descriptionl,
             ispublic
             FROM  book_list
             WHERE 
                 idusercreator = $1
-            `, [id]);
-        
-            const userLists = userListsQuery.rows;
+            `,
+      [id]
+    );
 
-        if (userLists.length === 0) {return res.status(400).json({ error: true, message: "No lists found" });}
+    const userLists = userListsQuery.rows;
 
-        res
-            .status(200)
-            .json({ error: false, message: "Lists found", data: userLists });
-
-            
-
-    } catch (error) {
-        // Handle the error here
-        console.error(error);
-        // Optionally, you can send an error response to the client
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (userLists.length === 0) {
+      return res.status(400).json({ error: true, message: "No lists found" });
     }
 
-
+    res
+      .status(200)
+      .json({ error: false, message: "Lists found", data: userLists });
+  } catch (error) {
+    // Handle the error here
+    console.error(error);
+    // Optionally, you can send an error response to the client
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
