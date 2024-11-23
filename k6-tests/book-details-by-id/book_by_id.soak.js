412d83ef02607a94b72d30296ad03859d5688bf7
jfmonsa <juanmonsalve@protonmail.com>
2024-11-23 09:07:26 -0500

    test: remove duration setting from soak test for book by id

--- k6-tests/book-details-by-id/book_by_id.soak.js
+++ k6-tests/book-details-by-id/book_by_id.soak.js
@@ -7,7 +7,6 @@ import getBookById from "./request.js";
 
 export const options = {
   vus: 10,
-  duration: "30m",
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
     http_req_duration: ["p(95)<200"], // 95% of requests should be below 200ms
