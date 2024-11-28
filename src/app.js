2113f10abf32e58b0abc178f84c6373fa4c39c98
jfmonsa <juanmonsalve@protonmail.com>
2024-11-28 00:39:39 -0500

    refactor: restructure app initialization and middleware setup for improved clarity and maintainability

--- src/app.js
+++ src/app.js
@@ -1,62 +1,25 @@
-import express from "express";
-import { setupDIContainer } from "./config/di-container.js";
+// import Server from "./app.js";
 
-// middlewares
-import cors from "cors";
-import corsOptions from "./config/cors.js";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
-import { formatResponse } from "./middlewares/formatResponse.js";
-import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
+// const server = new Server();
+// let app;
 
-// api
-import { swaggerDocs as swaggerDocsV1 } from "./config/swagger.js";
-import loadRoutes from "./api/router.js";
+// // initialize app (singleton)
+// const getApp = async () => {
+//   if (!app) {
+//     app = await server.create();
+//   }
+//   return app;
+// };
 
-class Server {
-  constructor() {
-    this.app = express();
-  }
+// // for local development
+// if (process.env.NODE_ENV !== "prod") {
+//   server.create().then(() => {
+//     server.start();
+//   });
+// }
 
-  async create() {
-    setupDIContainer();
-    await this.#setupMiddlewares();
-    return this.app;
-  }
-
-  async #setupMiddlewares() {
-    // -- Middlewares --
-    this.app.use(cors(corsOptions));
-    // support post requests
-    this.app.use(express.json());
-    // for form data
-    this.app.use(express.urlencoded({ extended: false }));
-    // support cookies
-    this.app.use(cookieParser());
-
-    // log requests
-    if (process.env.NODE_ENV === "dev") {
-      this.app.use(morgan("dev"));
-    }
-    // format response
-    this.app.use(formatResponse);
-
-    // Routes API Rest
-    this.app.use("/api/:version", await loadRoutes());
-
-    // handling errors
-    this.app.use(errorHandlerMiddleware);
-  }
-
-  // for local development
-  async start() {
-    const port = process.env.PORT;
-
-    this.app.listen(port, () => {
-      console.log(`-> 💻 Server running at: ${process.env.SERVER_URL}`);
-      swaggerDocsV1(this.app);
-    });
-  }
-}
-
-export default Server;
+// // handler for vercel
+// export default async function handler(req, res) {
+//   const app = await getApp();
+//   return app(req, res);
+// }
